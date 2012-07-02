b0b231930f0382742b7eace2d4f6287df1143caa
hauk142 <hauk142@gmail.com>
2012-07-02 22:49:47 +0200

    Initial commit

---
+++ src/com/hauk142/hackles/HacklesActivity.java
@@ -0,0 +1,15 @@
+package com.hauk142.hackles;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HacklesActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
