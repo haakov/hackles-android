2cb6e66eac70042f678d301fbb771ef8633d20ff
Håkon Vågsether <hauk142@gmail.com>
2012-08-19 00:16:33 +0200

    Buttons are debuggingly functional

--- src/com/hauk142/hackles/HacklesActivity.java
+++ src/com/hauk142/hackles/HacklesActivity.java
@@ -1,43 +1,58 @@
 package com.hauk142.hackles;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
 import android.os.Bundle;
 import android.widget.ImageView;
-import android.content.DialogInterface;
 import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.app.DialogFragment;
 import android.widget.Toast;
-
+import android.widget.Button;
+import android.view.View;
 
 public class HacklesActivity extends Activity
 {
+    int comic=1; // Which comic
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
     	setContentView(R.layout.main);
+	
 	ImageView image = (ImageView) findViewById(R.id.test_image);
-	downloadStrips();
-    }
+	Button Next = (Button) findViewById(R.id.ButtonNext);
+	Button Previous = (Button) findViewById(R.id.ButtonPrevious);
 
-    public void downloadStrips()
-    {
-	ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-	NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-
-	if( networkInfo != null && networkInfo.isConnected() )
+	Next.setOnClickListener(new Button.OnClickListener() 
 	{
-		
-	}
-	else
+		public void onClick(View v) {
+			try
+			{
+				// Do something!
+				if(comic != 364)
+					toast("Next!");
+				else
+					toast("Reached the last comic!");
+			}
+			catch (Exception e) {
+			}
+		}
+	});
+	Previous.setOnClickListener(new Button.OnClickListener()
 	{
-		toast("Connection error!");
-	}
+		public void onClick(View v) {
+			try
+			{
+				// Do something!
+				if(comic != 1)
+					toast("Previous!");
+				else
+					toast("Reached the first comic!");
+			}
+			catch (Exception e) {
+			}
+		}
+	});
     }
 
     public void toast(String text)
