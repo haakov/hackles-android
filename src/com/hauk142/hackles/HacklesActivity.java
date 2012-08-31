e25922efcdda47ca4a70dfdca351ae1120916d40
Håkon Vågsether <hauk142@gmail.com>
2012-08-31 17:43:50 +0200

    Added swipe/fling support

--- src/com/hauk142/hackles/HacklesActivity.java
+++ src/com/hauk142/hackles/HacklesActivity.java
@@ -11,17 +11,24 @@ import android.content.SharedPreferences;
 import android.widget.ImageView;
 import android.widget.Toast;
 import android.widget.Button;
-import android.view.View;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Environment;
+import android.view.GestureDetector;
+import android.view.GestureDetector.OnGestureListener;
+import android.view.MotionEvent;
+import android.view.View;
 
-
-public class HacklesActivity extends Activity
+public class HacklesActivity extends Activity implements OnGestureListener
 {
     int comic;
     ImageView image;
     SharedPreferences.Editor editor;
+    private GestureDetector gesture;
+	    
+    private static final int SWIPE_MIN_DISTANCE = 120;
+    private static final int SWIPE_MAX_OFF_PATH = 250;
+    private static final int SWIPE_THRESHOLD_VELOCITY = 200;
     
     /** Called when the activity is first created. */
     @Override
@@ -29,7 +36,7 @@ public class HacklesActivity extends Activity
     {
         super.onCreate(savedInstanceState);
     	setContentView(R.layout.main);
-	
+	gesture = new GestureDetector(this);
 	image = (ImageView) findViewById(R.id.image);
 	Button Next = (Button) findViewById(R.id.ButtonNext);
 	Button Previous = (Button) findViewById(R.id.ButtonPrevious);
@@ -117,5 +124,82 @@ public class HacklesActivity extends Activity
 		    e.printStackTrace();
 		    return null; 
 	    }
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+	    return gesture.onTouchEvent(event);
+    }
+    @Override
+    public boolean onDown(MotionEvent e) 
+    {
+	    return false;
+    }
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) 
+    {
+	    try
+	    {
+		    if( Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH )
+			    return false;
+
+		    // Left to right
+		    if( e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY )
+		    {
+				if(comic != 1)
+				{
+					comic--;
+					new DownloadImage().execute("http://hackles.org/strips/cartoon" + comic + ".png");
+					editor.putInt("comic", comic);
+					editor.commit();
+					toast(Integer.toString(comic));
+				}
+				else
+				{
+					toast("Reached the first comic!");
+				}
+		    }
+
+
+		    // Right to left
+		    else if( e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY )
+		    {
+				if(comic != 364)
+				{
+					comic++;
+					new DownloadImage().execute("http://hackles.org/strips/cartoon" + comic + ".png");
+					editor.putInt("comic", comic);
+					editor.commit();
+					toast(Integer.toString(comic));
+				}
+				else
+				{
+					toast("Reached the first comic!");
+				}
+		    }
+	    }
+	    catch (Exception e)
+	    {
+	    }
+
+	    return false;
+    }										 
+    @Override
+    public void onLongPress(MotionEvent e) 
+    {
+    }
+    @Override
+    public void onShowPress(MotionEvent e) 
+    {
+    }
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) 
+    {
+	    return false;
+    }
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY)
+    {
+	    return false;
     }
 }
